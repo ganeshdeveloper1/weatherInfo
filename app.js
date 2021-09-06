b0344de4ab56ed6cdb3608cae381aba4303f60f2
rohittiwari833 <rohittiwari7262@gmail.com>
2021-09-06 20:06:34 +0530

    changes completed

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
-})
+});
 
 
 
@@ -40,4 +41,4 @@ app.post("/", function(req, res) {
 
 app.listen(3000, function() {
     console.log("server is running on port 3000");
-})
+});
